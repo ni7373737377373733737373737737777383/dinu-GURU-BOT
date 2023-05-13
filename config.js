c65af8d90febec769d2ab4b1625629db6d89915d
ni7373737377373733737373737737777383 <133508874+ni7373737377373733737373737737777383@users.noreply.github.com>
2023-05-13 20:27:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94767369566', 'sandaru', true],
+  ['94767369566', 'Dinu', true], 
   [''] 
 ] //Number of owners
 
